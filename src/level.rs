7f8e3eab8d7e7ce67af59c90a1b535f64835c6de
pkh_anthone <pkh_anthone@photons.com.tw>
2024-05-06 10:26:38 +0800

    Change the logging color of critical
    [Chg] `level.rs`
    - [Chg] use `\x1b[38;5;214m` to print orange

--- src/level.rs
+++ src/level.rs
@@ -23,13 +23,13 @@ impl std::fmt::Display for Level {
       (Level::Error   , false) => write!(f, "[E]"),
       (Level::Fatal   , false) => write!(f, "[F]"),
 
-      (Level::Debug   , true ) => write!(f, "\x1b[90m[D]\x1b[0m"),  // Gray
-      (Level::Verbose , true ) => write!(f, "\x1b[90m[V]\x1b[0m"),  // Gray
-      (Level::Info    , true ) => write!(f, "\x1b[32m[I]\x1b[0m"),  // Green
-      (Level::Warning , true ) => write!(f, "\x1b[33m[W]\x1b[0m"),  // Yellow
-      (Level::Critical, true ) => write!(f, "\x1b[33m[C]\x1b[0m"),  // Yellow
-      (Level::Error   , true ) => write!(f, "\x1b[31m[E]\x1b[0m"),  // Red
-      (Level::Fatal   , true ) => write!(f, "\x1b[31m[F]\x1b[0m"),  // Red
+      (Level::Debug   , true ) => write!(f, "\x1b[90m[D]\x1b[0m"),        // Gray
+      (Level::Verbose , true ) => write!(f, "\x1b[90m[V]\x1b[0m"),        // Gray
+      (Level::Info    , true ) => write!(f, "\x1b[32m[I]\x1b[0m"),        // Green
+      (Level::Warning , true ) => write!(f, "\x1b[33m[W]\x1b[0m"),        // Yellow
+      (Level::Critical, true ) => write!(f, "\x1b[38;5;214m[C]\x1b[0m"),  // Orange
+      (Level::Error   , true ) => write!(f, "\x1b[31m[E]\x1b[0m"),        // Red
+      (Level::Fatal   , true ) => write!(f, "\x1b[31m[F]\x1b[0m"),        // Red
     }
   }
 }
