1dc5b1207d69b30b6de2a48e7556947bd064d1dd
pkh_anthone <pkh_anthone@photons.com.tw>
2024-05-06 13:54:13 +0800

    Add helper macros
    [Add] `macro.rs`
    - [Add] 7 new macros for different levels
    
    [Chg] `lib.rs`
    - [Add] include `macro.rs`
    - [Add] export everything from `macro.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod level;
 mod loggable;
 mod logger;
 mod session;
+mod r#macro;
 
 
 pub use error   ::*;
@@ -10,3 +11,4 @@ pub use level   ::Level;
 pub use loggable::Loggable;
 pub use logger  ::Logger;
 pub use session ::Session;
+pub use r#macro ::*;
