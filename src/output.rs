ff20c19917ffd31a6404ea576143652ee19dedec
TNThung <hung11112002@gmail.com>
2025-02-20 09:18:22 +0800

    [Add] implement Output trait and OutputInner trait as common interface for components

---
+++ src/output.rs
@@ -0,0 +1,38 @@
+
+
+/// Output trait is used to define a common interface for all types that can both be written to a file
+/// and printed to the console. This is the basic traits for each components of the log.
+pub trait Output {
+  /// Writes a string representation that is suitable for writing to a file.
+  fn for_write(&self, f: &mut impl std::fmt::Write);
+
+  /// Writes a string representation that is suitable for printing to the console.
+  fn for_print(&self, f: &mut impl std::fmt::Write);
+
+  /// Writes a string representation that is suitable for printing to the console when the color feature
+  /// is enabled. By default, this method is the same as `for_print`.
+  fn for_print_colored(&self, f: &mut impl std::fmt::Write) {
+    self.for_print(f);
+  }
+}
+
+
+pub(crate) trait OutputInner {
+  fn write(&self, f: &mut impl std::fmt::Write);
+  fn print(&self, f: &mut impl std::fmt::Write);
+}
+
+
+impl<T: Output> OutputInner for T {
+  fn write(&self, f: &mut impl std::fmt::Write) {
+    self.for_write(f);
+  }
+
+  fn print(&self, f: &mut impl std::fmt::Write) {
+    #[cfg(not(feature = "color"))]
+    self.for_print(f);
+
+    #[cfg(feature = "color")]
+    self.for_print_colored(f);
+  }
+}
