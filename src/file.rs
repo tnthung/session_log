8b31afccf3091bcb9d4817cab2019cb157048e05
TNThung <hung11112002@gmail.com>
2025-02-24 11:09:23 +0800

    [Chg] move unit constants to a separate module for better organization

--- src/file.rs
+++ src/file.rs
@@ -1,29 +1,10 @@
+use crate::unit::*;
 use crate::config::Config;
 use crate::logger::Logger;
 use std::io::Write;
 use std::fs::File as StdFile;
 use std::sync::{Arc, Mutex};
 use chrono::{Local, Timelike};
-
-
-#[allow(non_upper_case_globals)]
-pub const KiB: u64 = 1024;
-
-#[allow(non_upper_case_globals)]
-pub const MiB: u64 = 1024 * KiB;
-
-#[allow(non_upper_case_globals)]
-pub const GiB: u64 = 1024 * MiB;
-
-
-#[allow(non_upper_case_globals)]
-pub const Min: u32 = 60;
-
-#[allow(non_upper_case_globals)]
-pub const Hr: u32 = 60 * Min;
-
-#[allow(non_upper_case_globals)]
-pub const Day: u32 = 24 * Hr;
 
 
 /// Configuration for the logger.
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,11 @@ pub mod bundle;
 pub mod logger;
 pub mod file;
 pub mod config;
+pub mod unit;
 
 
 pub mod prelude {
   pub use crate::components::Level;
   pub use crate::config::Config;
   pub use crate::file::FileConfig;
-}
-
-pub mod unit {
-  pub use crate::file::{
-    KiB, MiB, GiB,  // File size
-    Day, Hr , Min,  // File duration
-  };
 }
