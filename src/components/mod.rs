6b1abbb00121207fcbd43fd94f726a236c7dd7e3
TNThung <hung11112002@gmail.com>
2026-05-06 14:02:36 +0800

    [Add] implement Component trait for log message handling

---
+++ src/components/mod.rs
@@ -0,0 +1,11 @@
+
+
+/// Components are building block of the log message.
+pub trait Component {
+  /// Used for non-color output, such as writing to a file.
+  fn write_plain(&self, f: &mut impl std::fmt::Write);
+
+  /// Used for color output, such as printing to the console.
+  #[cfg(feature = "color")]
+  fn write_color(&self, f: &mut impl std::fmt::Write) { self.plain(f); }
+}
