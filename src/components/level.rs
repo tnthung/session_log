f9a95f5d58931ce97978d299210c77def2c13786
TNThung <hung11112002@gmail.com>
2026-05-13 15:26:29 +0800

    [Add] expand level components with more formats

--- src/components/level.rs
+++ src/components/level.rs
@@ -2,11 +2,69 @@ use super::Component;
 use log::Level as LogLevel;
 
 
-/// A component that represents the log level.
+/// A component that represents the log level with a fixed width of 5 characters.
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Level;
 
 impl Component for Level {
+  fn write_plain<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
+    match record.level() {
+      LogLevel::Trace => write!(f, "[TRACE]"),
+      LogLevel::Debug => write!(f, "[DEBUG]"),
+      LogLevel::Info  => write!(f, "[INFO ]"),
+      LogLevel::Warn  => write!(f, "[WARN ]"),
+      LogLevel::Error => write!(f, "[ERROR]"),
+    }.unwrap();
+  }
+
+  #[cfg(feature = "color")]
+  fn write_color<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
+    use colored::Colorize;
+    write!(f, "{}", match record.level() {
+      LogLevel::Trace => "[TRACE]".bright_black(),
+      LogLevel::Debug => "[DEBUG]".white(),
+      LogLevel::Info  => "[INFO ]".green(),
+      LogLevel::Warn  => "[WARN ]".yellow(),
+      LogLevel::Error => "[ERROR]".red(),
+    }).unwrap();
+  }
+}
+
+
+/// A component that represents the log level without padding.
+#[derive(Debug, Default, Clone, Copy)]
+pub struct LevelNoPadding;
+
+impl Component for LevelNoPadding {
+  fn write_plain<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
+    match record.level() {
+      LogLevel::Trace => write!(f, "TRACE"),
+      LogLevel::Debug => write!(f, "DEBUG"),
+      LogLevel::Info  => write!(f, "INFO"),
+      LogLevel::Warn  => write!(f, "WARN"),
+      LogLevel::Error => write!(f, "ERROR"),
+    }.unwrap();
+  }
+
+  #[cfg(feature = "color")]
+  fn write_color<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
+    use colored::Colorize;
+    write!(f, "{}", match record.level() {
+      LogLevel::Trace => "TRACE".bright_black(),
+      LogLevel::Debug => "DEBUG".white(),
+      LogLevel::Info  => "INFO" .green(),
+      LogLevel::Warn  => "WARN" .yellow(),
+      LogLevel::Error => "ERROR".red(),
+    }).unwrap();
+  }
+}
+
+
+/// A component that represents the log level with a single character and brackets.
+#[derive(Debug, Default, Clone, Copy)]
+pub struct LevelCompact;
+
+impl Component for LevelCompact {
   fn write_plain<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
     match record.level() {
       LogLevel::Trace => write!(f, "[T]"),
@@ -26,6 +84,35 @@ impl Component for Level {
       LogLevel::Info  => "[I]".green(),
       LogLevel::Warn  => "[W]".yellow(),
       LogLevel::Error => "[E]".red(),
+    }).unwrap();
+  }
+}
+
+
+/// A component that represents the log level with a single character without brackets.
+#[derive(Debug, Default, Clone, Copy)]
+pub struct LevelMoreCompact;
+
+impl Component for LevelMoreCompact {
+  fn write_plain<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
+    match record.level() {
+      LogLevel::Trace => write!(f, "T"),
+      LogLevel::Debug => write!(f, "D"),
+      LogLevel::Info  => write!(f, "I"),
+      LogLevel::Warn  => write!(f, "W"),
+      LogLevel::Error => write!(f, "E"),
+    }.unwrap();
+  }
+
+  #[cfg(feature = "color")]
+  fn write_color<'a>(&self, f: &mut dyn std::io::Write, record: &log::Record<'a>) {
+    use colored::Colorize;
+    write!(f, "{}", match record.level() {
+      LogLevel::Trace => "T".bright_black(),
+      LogLevel::Debug => "D".white(),
+      LogLevel::Info  => "I".green(),
+      LogLevel::Warn  => "W".yellow(),
+      LogLevel::Error => "E".red(),
     }).unwrap();
   }
 }
