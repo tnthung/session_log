2d2d6ce68b00ecb7dc13651a59edcd0d046e39a5
TNThung <hung11112002@gmail.com>
2026-05-14 11:47:48 +0800

    [Add] creation for new session meta

--- src/session.rs
+++ src/session.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::time::Instant;
 use std::sync::{Mutex, LazyLock};
+use uuid::Uuid;
 
 
 #[derive(Debug, Clone)]
@@ -19,5 +20,25 @@ impl Metadata {
   pub fn get(sid: u128) -> Option<Metadata> {
     SESSION_METADATA.lock().unwrap()
       .get(&sid).cloned()
+  }
+
+  fn new(name: Option<String>, parent_id: Option<u128>) -> Self {
+    let nest_level = parent_id.as_ref()
+      .and_then(|pid| SESSION_METADATA.lock().unwrap()
+        .get(pid).map(|parent| parent.nest_level + 1))
+      .unwrap_or(1);
+
+    let ret = Self {
+      name,
+      parent_id,
+      nest_level,
+      current_id: Uuid::now_v7().to_u128_le(),
+      start_time: Instant::now(),
+    };
+
+    SESSION_METADATA.lock().unwrap()
+      .insert(ret.current_id, ret.clone());
+
+    ret
   }
 }
